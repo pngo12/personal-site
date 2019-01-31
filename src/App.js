4a49d6da3d0e9327d01d310eed012554582f0ba3
pngo12 <pngo12@gmail.com>
2019-01-31 10:07:09 -0800

    redid entire site

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 import About from './components/About';
-import Projects from './components/Projects';
-import Skills from './components/Skills';
-import Hero from './components/Hero';
 import Header from './components/Header';
-import Footer from './components/Footer';
 import './App.css';
 
 class App extends Component {
@@ -12,7 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <div className="flex flex-column items-center">
-          <div className="outline w-60 pv5 mv4">
+          <div className="outline w-60 pv3 mv2">
             <Header
               title="👋 Hello there!"
               subtitle="I'm Phillip Ngo 👨‍💻"
@@ -20,12 +16,12 @@ class App extends Component {
               technology "
             />
           </div>
-          <div className="outline w-60 pv5 mv1">
+          <div className="outline w-60">
             <About 
-            text="Typography has one plain duty before it and that is to convey information
-                in writing. No argument or consideration can absolve typography from this
-                duty. A printed work which cannot be read becomes a product without
-                purpose."
+            text="I studied Business and started my career in marketing. 
+              I was a full-time analyst before deciding to change career paths. 
+              I believe stagnation is a slow death so I'm always looking to better my skills and learn new ones.
+              I enjoy solving complicated problems and collaboration"
             />
       </div>
           <div className="outline w-60 pv5 mv3">
@@ -35,10 +31,6 @@ class App extends Component {
             4
       </div>
         </div>
-        {/* <Hero />
-        <Skills />
-        <Projects />
-        <Footer /> */}
       </div>
     );
   }
--- src/components/About.js
+++ src/components/About.js
@@ -4,11 +4,7 @@ const About = props => {
     return (
         <main class="pa3 pa5-ns">
             <p class="f4 lh-copy measure">
-            {props.text}
-                Typography has one plain duty before it and that is to convey information
-                in writing. No argument or consideration can absolve typography from this
-                duty. A printed work which cannot be read becomes a product without
-                purpose.
+                {props.text}
             </p>
         </main>
 
